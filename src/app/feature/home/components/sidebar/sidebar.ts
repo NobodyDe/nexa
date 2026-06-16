d69662c361847025cda9eef8c52f8ed010c0f36d
NobodyDe <henriquesantos.game@gmail.com>
2026-06-15 23:55:56 -0300

    feat: add auth feature, user service with signals, and new-post component

--- src/app/feature/home/components/sidebar/sidebar.ts
+++ src/app/feature/home/components/sidebar/sidebar.ts
@@ -1,16 +1,12 @@
-import { Component } from '@angular/core';
+import { UserService } from './../../../../core/services/user';
+import { Component, inject } from '@angular/core';
 import { LucideBellDot, LucideHouse, LucideSearch, type LucideIconData } from '@lucide/angular';
 import { IconButton } from '../../../../shared/components/icon-button/icon-button';
+import { User } from '../../../../core/models/user.model';
 
 interface SidebarMenuItem {
   icon: LucideIconData;
   label: string;
-}
-
-interface SidebarUser {
-  name: string;
-  profileName: string;
-  profilePic: string;
 }
 
 @Component({
@@ -20,18 +16,21 @@ interface SidebarUser {
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  private userService = inject(UserService);
   menuButtons: SidebarMenuItem[] = [
     { icon: LucideHouse.icon, label: 'Home' },
     { icon: LucideSearch.icon, label: 'Explorar' },
     { icon: LucideBellDot.icon, label: 'Notificações' },
   ];
 
-  user: SidebarUser = {
-    name: 'Guts',
-    profileName: '@GutsSocaFofo',
-    profilePic:
-      'https://avatars.githubusercontent.com/u/129473274?s=400&u=bd74e307af5334899f11af8035c5d3ee972c8cb7&v=4',
-  };
+  user = this.userService.user;
+
+  // user: User = {
+  //   name: 'Guts',
+  //   profileName: '@GutsSocaFofo',
+  //   profilePic:
+  //     'https://avatars.githubusercontent.com/u/129473274?s=400&u=bd74e307af5334899f11af8035c5d3ee972c8cb7&v=4',
+  // };
 
   onMenuClick(item: SidebarMenuItem): void {
     console.log('Menu clicado:', item.label);
