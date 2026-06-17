30f993c3a5f4c2f9f523904adbc3c8ce4daa8ffc
NobodyDe <henriquesantos.game@gmail.com>
2026-06-17 05:31:55 -0300

    feat(auth): implementa etapa de login com senha e decodificação do accessToken

--- src/app/core/models/auth.model.ts
+++ src/app/core/models/auth.model.ts
@@ -1,3 +1,18 @@
 export interface CheckEmailResponse {
-  exists: boolean;
+  emailExist: boolean;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface AccessTokenPayload {
+  sub: string;
+  name: string;
+  profileName: string;
+  profilePic: string;
+  email: string;
+  iat: number;
+  exp: number;
 }
--- src/app/core/services/user.ts
+++ src/app/core/services/user.ts
@@ -5,18 +5,15 @@ import { User } from '../models/user.model';
   providedIn: 'root',
 })
 export class UserService {
-  private userSignal = signal<User>({
-    name: 'Guts',
-    profileName: '@GutsOnlyReadOnly',
-    profilePic:
-      'https://avatars.githubusercontent.com/u/129473274?s=400&u=bd74e307af5334899f11af8035c5d3ee972c8cb7&v=4',
-  });
-  // Expõe o signal como readonly (somente leitura) para os consumidores
-  // Isso é um padrão de encapsulamento — ninguém de fora pode alterar o signal diretamente
+  private userSignal = signal<User | null>(null);
+
   readonly user = this.userSignal.asReadonly();
 
-  // Método para atualizar o usuário (quando precisar no futuro)
   updateUser(user: User): void {
     this.userSignal.set(user);
+  }
+
+  clearUser(): void {
+    this.userSignal.set(null);
   }
 }
